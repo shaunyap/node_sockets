d25e798ae755facc12a7d42fe3f5ced1ddb2b061
Shaun Yap <shaun@siegeflow.com>
2016-05-08 10:03:18 +0400

    show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,16 @@
 var socket = io();
 
-
 socket.on('connect', function() {
-    console.log('Connected to socket.io server.');
+    console.log('Conncted to socket.io server!');
 });
 
 socket.on('message', function(message) {
     var momentTimestamp = moment.utc(message.timestamp);
-    console.log('New message: ');
+    console.log('New message:');
     console.log(message.text);
 
     jQuery('.messages').append('<p>[' + momentTimestamp.local().format(
-            'h:mm a') + '] ' +
-        message.text + '</p>')
+        'h:mm a') + '] ' + message.text + '</p>');
 });
 
 // Handles submitting of new message
@@ -20,9 +18,12 @@ var $form = jQuery('#message-form');
 
 $form.on('submit', function(event) {
     event.preventDefault();
-    var $message = $form.find('input[name=message]')
+
+    var $message = $form.find('input[name=message]');
+
     socket.emit('message', {
         text: $message.val()
     });
+
     $message.val('');
-});;
+});
